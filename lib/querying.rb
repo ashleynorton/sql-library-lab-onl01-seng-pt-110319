4603b5d17fec877a102a5a6a7a85c9a5e78bb119
Ashley Norton <nortonashleym@gmail.com>
2020-02-19 06:17:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species) AS species_count FROM characters WHERE MAX(species_count);"
 end
 
 def select_name_and_series_subgenres_of_authors
